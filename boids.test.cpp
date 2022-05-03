11d25f644d4ae5e2d9bca7365ff63e105452e217
JoeMVent <104830361+JoeMVent@users.noreply.github.com>
2022-05-03 12:41:06 +0200

    Create boids.test.cpp

---
+++ boids.test.cpp
@@ -0,0 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "boids.hpp"
+
+#include "doctest.h"
+
+TEST_CASE("Testing boids.hpp") {
+  SUBCASE("Testing Sep") {}
+  SUBCASE("Testing All") {}
+  SUBCASE("Testing Coe") {}
+  SUBCASE("Testing Boids with 2 elements") {}
+  SUBCASE("Testing Boids with ... elements") {}
+}
