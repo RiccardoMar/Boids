50c8250f9869dd64854ea09bb45058e8e4c44cb7
Catalins01 <100802857+extter@users.noreply.github.com>
2022-05-10 12:40:23 +0200

    Add operator ()

--- neighbours.check.hpp
+++ neighbours.check.hpp
@@ -1,16 +1,20 @@
-#IFNDEF NEIGHBOURS.CHECK_HPP
-#DEFINE NEIGHBOURS.CHECK_HPP
+#ifndef NEIGHBOURS_CHECK_HPP
+#define NEIGHBOURS_CHECK_HPP
 
 
 #include <cmath>
 
-#include "Ustate.hpp"
+#include "UState.hpp"
 
 
-struct Check {
-  bool operator()(Ustate const& u1, UState const& u2) const {
-      return abs((u1.x^2 + u1.y^2)^0.5 - (u2.x^2 + u2.y^2)^0.5)
-  }
+class Check {
+double d_;
+public:
+Check(double const d) : d_(d){}
+
+bool operator()(Ustate const& u1, UState const& u2) const {
+    return abs((u1.x^2 + u1.y^2)^0.5 - (u2.x^2 + u2.y^2)^0.5) < d;
+}
 }
 
 
