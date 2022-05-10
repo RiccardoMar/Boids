bc0a14882725e9930f8e2dc7877cabb683c12a56
Catalins01 <100802857+extter@users.noreply.github.com>
2022-05-10 12:29:53 +0200

    Add struct Check

--- neighbours.check.hpp
+++ neighbours.check.hpp
@@ -1,8 +1,21 @@
-#ifndef NEIGHBOURS_HPP
-#define NEIGHBOURS_HPP
+#IFNDEF NEIGHBOURS.CHECK_HPP
+#DEFINE NEIGHBOURS.CHECK_HPP
 
-class Check {
 
- }
+#include <cmath>
 
+#include "Ustate.hpp"
+
+
+struct Check {
+  bool operator()(Ustate const& u1, UState const& u2) const {
+      return abs((u1.x^2 + u1.y^2)^0.5 - (u2.x^2 + u2.y^2)^0.5)
+  }
+}
+
+
+
+
+//qual è la distanza limite per la definizione di un vicino? 
+//siccome il check prende due punti e vede se sono vicini, non ha var private -> lo implemento come struct?
 #endif
