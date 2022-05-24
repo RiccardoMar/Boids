7b9e9edae99fc35e8e0ff385ac6e05688cc3f1a2
JoeMVent <104830361+JoeMVent@users.noreply.github.com>
2022-05-24 11:56:57 +0200

    Aggiornato Ustate.cpp

--- UState.cpp
+++ UState.cpp
@@ -5,6 +5,6 @@
 bool UState::operator==(UState const other) const {}
 
 std::ostream& operator<<(std::ostream& os, UState const& u) {
-  os << "Posizione: (" <<u.x << ", " <<u.y <<") \n Velocità: (" << u.vx << ", " <<u.vy <<')';
+  os << "Posizione: (" <<u.P.x << ", " <<u.P.y <<") \n Velocità: (" << u.V.vx << ", " <<u.V.vy <<')';
   return os;
 }
