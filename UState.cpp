226071eb1bc219cd9023f0d7e6fc289880041bab
JoeMVent <104830361+JoeMVent@users.noreply.github.com>
2022-05-24 12:16:36 +0200

    Implementata funzione distanza

--- UState.cpp
+++ UState.cpp
@@ -1,8 +1,18 @@
 #include "UState.hpp"
 
 #include <iostream>
+#include <cmath>
 
-bool UState::operator==(UState const other) const {}
+bool UState::operator==(UState const& other) const {}
+
+double UState::dist(UState const& other) const {
+  double d = sqrt((P.x-other.P.x)*(P.x-other.P.x)+(P.y-other.P.y)*(P.y-other.P.y));
+  return d;
+}
+
+double dist(UState const& u1, UState const& u2) {
+  return u1.dist(u2);
+}
 
 std::ostream& operator<<(std::ostream& os, UState const& u) {
   os << "Posizione: (" <<u.P.x << ", " <<u.P.y <<") \n Velocità: (" << u.V.vx << ", " <<u.V.vy <<')';
--- UState.hpp
+++ UState.hpp
@@ -16,8 +16,11 @@ struct Velocity {
 struct UState { // stato dei singoli uccelli. Posizione e velocità in x,y
   Position P{};
   Velocity V{};
-  bool operator==(UState const other) const; //per i test
+  bool operator==(UState const& other) const; //per i test
+  double dist(UState const& other) const;
 };
+
+double dist(UState const& u1, UState const& u2)
 
 std::ostream& operator<<(std::ostream& os, UState const& u);
 
