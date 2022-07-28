3915e69670a7d0fc4fbef2a7c0965a68315ab280
JoeMVent <104830361+JoeMVent@users.noreply.github.com>
2022-07-28 13:00:13 +0200

    Implementazione Sep

--- flight.rules.hpp
+++ flight.rules.hpp
@@ -10,14 +10,18 @@ double const ds = 3;
 
 public:
 Sep(double s, double d_s) : S{s}, ds{d_s} {};
-
-double operator()(UState const& u1, UState const& u2) const {
-     if( std::abs(u1.P.x-u2.P.x) < ds){
-       return -S * (u2.P.x - u1.P.x);
-      } else {
-        return 0;
-      }
-};
+std::vector<Velocity> operator()(std::vector<Coppia> const& Vicini) const {
+  for (int i = 0; i != Vicini.size(); ++i) {
+    if(dist(Vicini[i].u1, Vicini[i].u2 < ds)){
+      Velocities[Vicini.u1.UPN] += uccelli[Vicini.u2.UPN].P - uccelli[Vicini.u1.UPN].P;
+      Velocities[Vicini.u2.UPN] += uccelli[Vicini.u1.UPN].P - uccelli[Vicini.u2.UPN].P;
+    }
+  }
+  for (int i = 0; i != Velocities.size(); ++i) {
+    (Velocities[i]-uccelli[i].P)/(-1/S);
+    }
+  return Velocities;
+}
 };
 
 // Allineamento
@@ -26,7 +30,7 @@ class All {
 
  public:
  All(double a) : A{a} {};
- std::vector<Velocity> operator()(std::vector<Coppia> Vicini){
+ std::vector<Velocity> operator()(std::vector<Coppia> const& Vicini) const {
   std::vector<Velocity> Velocities(uccelli.size());
   std::vector<short int> Counters(uccelli.size());
   for (int i = 0; i != Vicini.size(); ++i) {
@@ -38,25 +42,32 @@ class All {
   for (int i = 0; i != Velocities.size(); ++i) {
     Velocities[i]/Counters[i]; //trasforma la sommatoria in media
     Velocities[i]/(1/A); //Nella struct Velocity ho implementato solo la divisione, quindi per moltiplicare divido per il reciproco.
-  }
-
+    }
+  return Velocities;
   };
 };
+
 // Coesione
 class Coe {
   double C;
 
  public:
   Coe(double c) : C{c} {};
-  std::vector<Velocity> operator()(std::vector<Coppia> Vicini){
-    std::vector<Velocity> Velocities(uccelli.size());
+  std::vector<Velocity> operator()(std::vector<Coppia> const& Vicini) const {
+    std::vector<Position> Velocities(uccelli.size());
     std::vector<short int> Counters(uccelli.size());
   for (int i = 0; i != Vicini.size(); ++i) {
-    
+    Velocities[Vicini.u1.UPN] += uccelli[Vicini.u2.UPN].P;
+    Velocities[Vicini.u2.UPN] += uccelli[Vicini.u1.UPN].P;
+    Counters[Vicini.u1.UPN]++;
+    Counters[Vicini.u2.UPN]++;
     }
-
+  for (int i = 0; i != Velocities.size(); ++i) {
+    Velocities[i]/Counters[i]; //trasforma la sommatoria in media
+    (Velocities[i]-uccelli[i].P)/(1/C);
+    }
+  return Velocities;
   };
-  
 };
 
 #endif
