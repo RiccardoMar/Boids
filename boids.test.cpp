b010f4b894eedd4472d8e0d909828b584ebda1a6
Catalin Sajin <catalin.sajin@studio.unibo.it>
2022-07-28 12:12:00 +0200

    Updated, waiting for const qualifier

--- boids.test.cpp
+++ boids.test.cpp
@@ -17,11 +17,21 @@ TEST_CASE("Testing boids.hpp") {
       CHECK_THROWS(Sep(-4, 0));
       CHECK_THROWS(Sep(0, -3));
     }
-    //per quanto riguarda la validazione dell'input (S = £ o S = ciao) -> mettere degli assert nel codice? negli hpp?
-    SUBCASE("Test funzionamento Sep")
+    // per quanto riguarda la validazione dell'input (S = £ o S = ciao) ->
+    // mettere degli assert nel codice? negli hpp?
+    Sep const sep{3.0, 3.0};
+    SUBCASE("Test funzionamento Sep") { // Cosa succede se metto ds non uguale a
+                                       // 3? percjé è costante in
+                                       // flightrules.hpp, invece in questo modo
+                                       // potrebbe lamentarsi {
+
+    UState u1{5, 5, 10, 10};
+    UState u2{7, 7, 10, 10};
+    CHECK(sep(u1, u2) == doctest::Approx(8.485).epsilon(0.01));}
   }
-  SUBCASE("Testing All") {}
-  SUBCASE("Testing Coe") {}
-  SUBCASE("Testing Boids with 2 elements") {}
-  SUBCASE("Testing Boids with ... elements") {}
+}
+SUBCASE("Testing All") {}
+SUBCASE("Testing Coe") {}
+SUBCASE("Testing Boids with 2 elements") {}
+SUBCASE("Testing Boids with ... elements") {}
 }
