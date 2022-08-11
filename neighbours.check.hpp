ed6fc9663b285fd7980f80d00a53af5e63539349
Catalin Sajin <catalin.sajin@studio.unibo.it>
2022-08-11 11:08:26 +0200

    Fixed some things

--- neighbours.check.hpp
+++ neighbours.check.hpp
@@ -9,6 +9,9 @@
 struct Coppia {
   UState u1;
   UState u2;
+
+//HA BISOGNO DI COSTRUTTORE????
+//U1 E U2 DEVONO ESSERE PASSATI BY REFERENCE??
 };
 
 std::vector<Coppia> Check(std::vector<UState> const& uccelli,
@@ -16,8 +19,6 @@ std::vector<Coppia> Check(std::vector<UState> const& uccelli,
   std::vector<Coppia> Vicini;
   for (int i = 0; i != uccelli.size() - 1; ++i) {
     for (int j = i; j != uccelli.size(); ++j) {
-      // double a = std::pow((uccelli[i].P.x - uccelli[j].P.x), 2) +
-      //            std::pow((uccelli[i].P.y - uccelli[j].P.y), 2);
       if (dist(uccelli[i], uccelli[j]) < distanza * distanza) {
         Vicini.push_back(Coppia{uccelli[i], uccelli[j]});
       }
