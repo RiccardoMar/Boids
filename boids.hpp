80265b1c12b1eb0be3d7c9628401185f9e4abfa5
Catalin Sajin <catalin.sajin@studio.unibo.it>
2022-08-16 10:06:47 +0200

    updated includes

--- boids.hpp
+++ boids.hpp
@@ -1,30 +1,30 @@
 #ifndef BOIDS_HPP
 #define BOIDS_HPP
-#include "UState.hpp"
-#include "neighbours.check.hpp"
-#include "flight.rules.hpp"
-
+//#include "UState.hpp" : forse si può togliere perché c'è in neighbours
+#include <cassert>
+#include <iostream>
+#include <random>
+#include <stdexcept>
 #include <vector>
 
+#include "flight.rules.hpp"
+#include "neighbours.check.hpp"
+
 class Boids {
+  std::vector<UState> UState_;
 
-std::vector<UState> UState_;
+  Sep sep_;
 
-Sep sep_;
+  All all_;
 
-All all_;
+  Coe coe_;
 
-Coe coe_;
+ public:
+  Boids(std::vector<UState> UState, Sep sep, All all, Coe coe)
+      : UState_{UState}, sep_{sep}, all_{all}, coe_{coe} {
+    // assertions here?
+  }
 
-public:
+};  // classe per applicare le regole di interazione a tutto lo stormo
 
-Boids(std::vector<UState> UState, Sep sep, All all, Coe coe) : UState_{UState}, sep_{sep} , all_{all}, coe_{coe}{
-//assertions here?
-}
-
-
-
-
-}; // classe per applicare le regole di interazione a tutto lo stormo
-
-#endif 
+#endif
