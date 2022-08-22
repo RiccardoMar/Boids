783b161ae7d52c0b97ea028c54a28dedce55f824
Catalin Sajin <catalin.sajin@studio.unibo.it>
2022-08-22 15:50:11 +0200

    updated

--- boids.hpp
+++ boids.hpp
@@ -1,14 +1,13 @@
 #ifndef BOIDS_HPP
 #define BOIDS_HPP
 //#include "UState.hpp" : forse si può togliere perché c'è in neighbours
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Time.hpp>
 #include <cassert>
 #include <iostream>
 #include <random>
 #include <stdexcept>
 #include <vector>
-#include <SFML/Graphics.hpp>
-#include <SFML/System/Time.hpp>
-
 
 #include "flight.rules.hpp"
 //#include "neighbours.check.hpp"
@@ -25,15 +24,12 @@ class Boids {
   int distance_;
 
  public:
-  Boids(std::vector<UState> UState, Sep const& sep, All const& all, Coe const& coe, int distance) //non devo mettere const UState poiché, dopo evolve, devo prendere il nuovo vettore e fare l'assignment UState = nuovo_vettore?
-      : UState_{UState}, sep_{sep}, all_{all}, coe_{coe}, distance_{distance} {
-  }
+  Boids(std::vector<UState> UState, Sep const& sep, All const& all,
+        Coe const& coe, int distance)
+      : UState_{UState}, sep_{sep}, all_{all}, coe_{coe}, distance_{distance} {}
 
-  std::vector<UState> const& state() const
-  {
-    return UState_;
-  } 
-  
+  std::vector<UState> const& state() const { return UState_; }
+
   void evolve(double delta_t);
 
 };  // classe per applicare le regole di interazione a tutto lo stormo
