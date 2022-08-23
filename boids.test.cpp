8eba951064b4a7bfdf9a7bb4d63032f46c6ead1a
Catalin Sajin <catalin.sajin@studio.unibo.it>
2022-08-23 16:03:45 +0200

    preparing for testing

--- boids.test.cpp
+++ boids.test.cpp
@@ -17,21 +17,32 @@ TEST_CASE("Testing Sep") {
   // per quanto riguarda la validazione dell'input (S = £ o S = ciao) ->
   // mettere degli assert nel codice? negli hpp?
   Sep const sep{3.0, 3.0};
-  SUBCASE("Test funzionamento Sep") {  // Cosa succede se metto ds non uguale a
-                                       // 3? percjé è costante in
-                                       // flightrules.hpp, invece in questo modo
-                                       // potrebbe lamentarsi {
+  SUBCASE("Test funzionamento Sep") {
+    UState u1;
+    u1.P.x = 5.;
+    u1.P.y = 5.;
+    u1.V.vx = 10.;
+    u1.V.vy = 10.;
+    u1.UPN = 1;
+    UState u2;
+    u2.P.x = 5.;
+    u2.P.y = 5.;
+    u2.V.vx = 10.;
+    u2.V.vy = 10.;
+    u2.UPN = 2;
 
-    UState u1{5, 5, 10, 10};
-    UState u2{7, 7, 10, 10};
-    CHECK(sep(u1, u2) == doctest::Approx(8.485).epsilon(0.01));
+    std::vector<UState> uccelli{u1, u2};
+    std::cout << "Primo uccello:" << uccelli[0];
+    std::cout << "Secondo uccello:" << uccelli[1];
+
+   // CHECK(sep(u1, u2) == doctest::Approx(8.485).epsilon(0.01));
   }
 }
 
-/*Per quanto riguarda la parte sopra, bisogna implementare dentro flightrules i seguenti runtime errors, simili a quelli del prof: 
-Hooke(double k, double l) : m_k(k), m_l(l) {
-    if (m_k <= 0.) {
-      throw std::runtime_error{"Hooke's parameter k must be > 0"};
+/*Per quanto riguarda la parte sopra, bisogna implementare dentro flightrules i
+seguenti runtime errors, simili a quelli del prof: Hooke(double k, double l) :
+m_k(k), m_l(l) { if (m_k <= 0.) { throw std::runtime_error{"Hooke's parameter k
+must be > 0"};
     }
     if (m_l < 0.) {
       throw std::runtime_error{"Hooke's parameter l must be >= 0"};
@@ -41,10 +52,7 @@ Hooke(double k, double l) : m_k(k), m_l(l) {
 
 TEST_CASE("Testing All") {}
 TEST_CASE("Testing Coe") {
-  SUBCASE("Testing ") {
-
-  }
+  SUBCASE("Testing ") {}
 }
 TEST_CASE("Testing Boids with 2 elements") {}
 TEST_CASE("Testing Boids with ... elements") {}
-
